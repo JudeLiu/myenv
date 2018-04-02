28c933e3ca545f9cd3c85034ad81d99e2d4129ff
ljn <ljnsjtu@hotmail.com>
2018-04-02 12:52:27 +0800

    a startup script for a fresh Ubuntu

---
+++ startup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# compilers & interpreters
+sudo apt-get install -y gcc clang
+
+sudo apt-get install -y python-pip python3-pip
+
+# ipython-notebook
+sudo apt-get install -y ipython-notebook
+
+
+
