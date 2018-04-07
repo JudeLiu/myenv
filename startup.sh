d4b43e164f23bd2894dbf2c5af9503e702272f1b
liujunnan <ljnsjtu@hotmail.com>
2018-04-07 17:30:49 +0800

    add new features

--- startup.sh
+++ startup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# install sublime text 3
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+
 # compilers & interpreters
 sudo apt-get install -y gcc clang
 
@@ -13,4 +20,6 @@ git config --global core.editor "'C:/Program Files/Sublime Text 2/sublime_text.e
 git config --global push.default upstream
 git config --global merge.conflictstyle diff3
 
+cp git-completion.bash ~/.git-completion.bash
+cp git-prompt.sh ~/.git-prompt.sh
 cat bash_profile_course >> ~/.bashrc
