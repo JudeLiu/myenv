48cf22d7b97a8cd0ecd32c2087fef68536e33197
Liu Junnan <ljnsjtu@hotmail.com>
2018-04-07 14:03:06 +0800

    add support for git in bash

--- startup.sh
+++ startup.sh
@@ -8,5 +8,9 @@ sudo apt-get install -y python-pip python3-pip
 # ipython-notebook
 sudo apt-get install -y ipython-notebook
 
+# git configs
+git config --global core.editor "'C:/Program Files/Sublime Text 2/sublime_text.exe' -n -w"
+git config --global push.default upstream
+git config --global merge.conflictstyle diff3
 
-
+cat bash_profile_course >> ~/.bashrc
