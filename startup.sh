143570b7d0f23af864ef3fc3c80cbc687e8585c5
Liu Junnan <ljnsjtu@hotmail.com>
2018-04-11 11:17:52 +0800

    change the order of code

--- startup.sh
+++ startup.sh
@@ -7,6 +7,20 @@ sudo cp sources.list /etc/apt/
 sudo apt-get update
 echo "done"
 
+# git configs
+echo "Configuring git..." 
+git config --global core.editor "subl"
+git config --global push.default upstream
+git config --global merge.conflictstyle diff3
+git config --global user.email "ljnsjtu@hotmail.com"
+git config --global user.name "liujunnan"
+git config credential.helper 'cache --timeout=3600'
+
+cp git-completion.bash ~/.git-completion.bash
+cp git-prompt.sh ~/.git-prompt.sh
+cat bash_profile_course >> ~/.bashrc
+echo "done"
+
 # install sublime text 3
 echo "Installing Sublime Text 3..."
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
@@ -32,18 +46,4 @@ sudo apt-get install -y ipython-notebook
 sudo pip install numpy
 sudo pip3 install numpy
 
-echo "done"
-
-# git configs
-echo "Configuring git..." 
-git config --global core.editor "subl"
-git config --global push.default upstream
-git config --global merge.conflictstyle diff3
-git config --global user.email "ljnsjtu@hotmail.com"
-git config --global user.name "liujunnan"
-git config credential.helper 'cache --timeout=3600'
-
-cp git-completion.bash ~/.git-completion.bash
-cp git-prompt.sh ~/.git-prompt.sh
-cat bash_profile_course >> ~/.bashrc
 echo "done"
