aa38f4f213080eb692f5dc7eacf1dae2372a9970
Liu Junnan <ljnsjtu@hotmail.com>
2018-04-11 11:16:19 +0800

    add message shown on terminal when the script running

--- startup.sh
+++ startup.sh
@@ -1,27 +1,41 @@
 #!/bin/bash
 
+# update sources.list
+echo "Updating sources.list..."
+sudo mv /etc/apt/sources.list /etc/apt/sources.list.old
+sudo cp sources.list /etc/apt/
+sudo apt-get update
+echo "done"
+
 # install sublime text 3
+echo "Installing Sublime Text 3..."
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-text
+echo "done"
 
 # compilers & interpreters
+echo "Installing clang & python..."
 sudo apt-get install -y gcc clang
 
 sudo apt-get install -y python
 sudo apt-get install -y python-pip python3-pip
 
-echo -e "\nalias py=python" >> ~/.bashrc
-echo -e "alias py3=python3" >> ~/.bashrc
+echo "done"
 
 # ipython-notebook
+echo "Installing python libs..."
 sudo apt-get install -y ipython-notebook
 # numpy
 sudo pip install numpy
+sudo pip3 install numpy
+
+echo "done"
 
 # git configs
+echo "Configuring git..." 
 git config --global core.editor "subl"
 git config --global push.default upstream
 git config --global merge.conflictstyle diff3
@@ -32,3 +46,4 @@ git config credential.helper 'cache --timeout=3600'
 cp git-completion.bash ~/.git-completion.bash
 cp git-prompt.sh ~/.git-prompt.sh
 cat bash_profile_course >> ~/.bashrc
+echo "done"
