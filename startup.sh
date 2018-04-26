6b9cf3ebfec957737fe103406009a544a514bfae
Liu Junnan <ljnsjtu@hotmail.com>
2018-04-26 17:16:56 +0800

    replace the source of conda with THU's mirror sites

--- startup.sh
+++ startup.sh
@@ -54,4 +54,10 @@ echo "done"
 echo "Installing miniconda"
 wget -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 Miniconda3-latest-Linux-x86_64.sh
+
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge 
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/msys2/
+
+conda config --set show_channel_urls yes
 echo "done"
