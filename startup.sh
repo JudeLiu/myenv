a3c96a43102b6d31d5a0d2b54d571db757bdba6e
liujunnan <ljnsjtu@hotmail.com>
2018-06-05 10:16:50 +0800

    remove sublime text

--- startup.sh
+++ startup.sh
@@ -20,21 +20,6 @@ cp git-prompt.sh ~/.git-prompt.sh
 cat bash_profile_course >> ~/.bashrc
 echo "done"
 
-# install sublime text 3
-echo "Installing Sublime Text 3..."
-uname -v | sudo grep Microsoft > /dev/null
-
-if [ $? -eq 0 ]; then
-	echo "Can't install sublime text on terminal on Windows Linux Subsystem"
-else
-	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-	sudo apt-get install apt-transport-https
-	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-	sudo apt-get update
-	sudo apt-get install sublime-text
-fi
-echo "done"
-
 
 # c++ compiler
 echo "Installing clang..."
