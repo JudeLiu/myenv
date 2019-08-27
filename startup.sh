2edf40f8332b9f2be4540d1e2331a68cfd4b4026
Liu Junnan <liujunnan@sjtu.edu.com>
2019-08-27 09:03:50 +0000

    Change zshrc
    using correct user path

--- startup.sh
+++ startup.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+
+read -p "Update sources.list? [Y/n] " ans 
+if [ "$ans" = "" ] || [ "$ans" = "Y" ] || [ "$ans" = "y" ]; then
+    # update sources.list
+    distro=`lsb_release -si`
+    codename=`lsb_release -sc`
+    if [ "$distro" = "Ubuntu" ]; then
+        echo "Updating sources.list..."
+        sed "s/xenial/${codename}/g" -i sources.list 
+        sudo mv /etc/apt/sources.list /etc/apt/sources.list.old
+        sudo cp sources.list /etc/apt/
+        sudo apt-get update
+        echo "done"
+    fi
+fi
+
 # git configs
 echo "Configuring git..." 
+sudo apt-get install git
 git config --global core.editor vim
 git config --global push.default upstream
 git config --global merge.conflictstyle diff3
@@ -11,7 +28,6 @@ git config credential.helper 'cache --timeout=7200'
 
 read -p "Install vim plugins? [Y/n] " ans
 if [ "$ans" = "" ] || [ "$ans" = "Y" ] || [ "$ans" = "y" ]; then
-
     # Install vim plugins
     echo "Install Vim plugins"
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -22,18 +38,16 @@ fi
 
 read -p "Install zsh? [Y/n] " ans
 if [ "$ans" = "" ] || [ "$ans" = "Y" ] || [ "$ans" = "y" ]; then
+    sudo apt-get install -y zsh
+    sudo chsh -s $(which zsh)
     sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     cp dotfiles/zshrc ~/.zshrc
-fi
-
-read -p "Sudo permission? [Y/n] " ans 
-if [ "$ans" = "" ] || [ "$ans" = "Y" ] || [ "$ans" = "y" ]; then
-    # update sources.list
-    echo "Updating sources.list..."
-    sudo mv /etc/apt/sources.list /etc/apt/sources.list.old
-    sudo cp sources.list /etc/apt/
-    sudo apt-get update
-    echo "done"
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone git://github.com/wting/autojump.git
+    cd autojump && ./install.py
+    echo >> ~/.bashrc
+    echo "# set default shell as zsh" >> ~/.bashrc 
+    echo "bash -c zsh" >> ~/.bashrc
 fi
 
 read -p "Install Miniconda? [Y/n] " ans 
@@ -41,7 +55,7 @@ if [ "$ans" = "" ] || [ "$ans" = "Y" ] || [ "$ans" = "y" ]; then
     # Fix matplotlib dependency
     # "libGl.so"
     sudo apt install -y libgl1-mesa-glx
-
+    echo "plz manually download from https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
     echo -e "If you want to show GUI on Windows Linux Subsystem, download Xming on Internet, and type
     touch ~/.config/matplotlib/matplotlibrc && echo \"backend: TkAgg\" >> ~/.config/matplotlib/matplotlibrc
     echo DISPLAY=localhost:0.0 >> ~/.bashrc"
